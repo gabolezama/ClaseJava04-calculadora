8f143ece76dae739be30aa0a37be09377fda1c25
EducaciónIT <EducacinIT@Instructor-605>
2018-03-16 22:03:16 -0300

    initial

--- src/main/java/dario/java/std/calculadoraoop/Calculadora.java
+++ src/main/java/dario/java/std/calculadoraoop/Calculadora.java
@@ -3,50 +3,79 @@ package dario.java.std.calculadoraoop;
 import java.util.Scanner;
 
 public class Calculadora {
-    
+
     private Scanner scanner;
     private Memoria memoria;
     private double buffer;
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-    
+
         String input;
+        boolean calculoExtendido;
+        Double calculoIntermedio;
         while (true) {
-            String operandoA = scanner.nextLine();        
-            String tipoOperacion = scanner.nextLine();
-            String operandoB = scanner.nextLine();
 
-            Operacion operacion=null; 
-            switch (tipoOperacion) {
-                case "+":
-                    operacion = new Suma();
-                    break;
-                case "-":
-                    operacion = new Resta();
-                    break;
-                case "/":
-                    operacion = new Division();
-                    break;               
-                case "*":
-                    operacion = new Multiplicacion();
-                    break;
-                case "p":
-                    operacion = new Potencia();
-                    break;
-                case "r":
-                    operacion = new Raiz();
-            }
-            
-            operacion.setOperandoA(Double.valueOf(operandoA));
-            operacion.setOperandoB(Double.valueOf(operandoB));
-        
-            System.out.println(operacion.operar());
+            // Inicio un calculo nuevo
+            String tipoOperacion = "";
+            calculoExtendido = false;
+            calculoIntermedio = 0.0;
 
-            
+            do {
+
+                if (!calculoExtendido) {
+                    String operandoA = scanner.nextLine();
+                    tipoOperacion = scanner.nextLine();
+                    String operandoB = scanner.nextLine();
+
+                    Operacion operacion = obtenerOperacion(tipoOperacion);
+                    operacion.setOperandoA(Double.valueOf(operandoA));
+                    operacion.setOperandoB(Double.valueOf(operandoB));
+
+                    calculoIntermedio = operacion.operar();
+                    calculoExtendido = true;
+                } else {
+                    tipoOperacion = scanner.nextLine();
+
+                    if (tipoOperacion.equals("=")) {
+                        System.out.println(calculoIntermedio);
+                    } else {
+                        String operandoB = scanner.nextLine();
+                        Operacion operacion = obtenerOperacion(tipoOperacion);
+                        operacion.setOperandoA(calculoIntermedio);
+                        operacion.setOperandoB(Double.valueOf(operandoB));
+                        calculoIntermedio = operacion.operar();
+                    }
+                }
+
+            } while (tipoOperacion != "=");
+
         }
-        
-        
+
     }
-    
+
+    private static Operacion obtenerOperacion(String tipoOperacion) {
+        Operacion operacion = null;
+        switch (tipoOperacion) {
+            case "+":
+                operacion = new Suma();
+                break;
+            case "-":
+                operacion = new Resta();
+                break;
+            case "/":
+                operacion = new Division();
+                break;
+            case "*":
+                operacion = new Multiplicacion();
+                break;
+            case "p":
+                operacion = new Potencia();
+                break;
+            case "r":
+                operacion = new Raiz();
+        }
+        return operacion;
+    }
+
 }

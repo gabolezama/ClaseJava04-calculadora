e2491aa5ab5d6f39ad608629d8e4f27daa992aae
EducaciónIT <EducacinIT@Instructor-802>
2018-03-26 20:34:01 -0300

    calculator ui refactor

--- src/main/java/dario/java/std/calculadoraoop/ui/CalculatorFX.java
+++ src/main/java/dario/java/std/calculadoraoop/ui/CalculatorFX.java
@@ -10,8 +10,7 @@ public class CalculatorFX extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
+        Parent root =  FXMLLoader.load(getClass().getClassLoader().getResource("FXMLDocument.fxml"));
         Scene scene = new Scene(root);
         
         stage.setScene(scene);

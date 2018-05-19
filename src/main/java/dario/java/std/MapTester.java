be91de88a7f09f68bffb2d666846a6306eb9815b
EducaciónIT <EducacinIT@Instructor-607>
2018-05-19 19:02:57 -0300

    palimdromo

--- src/main/java/dario/java/std/MapTester.java
+++ src/main/java/dario/java/std/MapTester.java
@@ -20,6 +20,25 @@ public class MapTester {
 
 		// write again to command line
 		map.forEach((k, v) -> System.out.printf("%s %s%n", k, v));
+                
+                
+                Map<String, Persona> personas = new HashMap<>();
+                personas.put("1111111",  new Persona("Juan", "Pérez", "1111111"));
+                personas.put("2222222", new Persona("Juan", "Gómez", "2222222"));
+                personas.put("1111111", new Persona("Juan", "Gómez", "1111111"));
+                personas.put("2222222", new Persona("Hector", "Peralta", "2222222"));
+                personas.put("33333333", new Persona("Darío", "Hruszecki", "33333333"));
+                personas.put("33333334", new Persona("Nicolás", "Hruszecki", "33333334"));
+                personas.put("33333335", new Persona("Norberto", "Hruszecki", "33333335"));
+                
+                personas.putIfAbsent("33333335", new Persona("Norberto", "Hruszecki", "33333335"));
+                
+                personas.replace("k", new Persona("Norberto", "Hruszecki", "33333335"));
+                
+                Persona p = personas.get("444");
+                
+                personas.getOrDefault("44444", new Persona("Norberto", "Hruszecki", "33333335"));
+                
 	
 	}
 
